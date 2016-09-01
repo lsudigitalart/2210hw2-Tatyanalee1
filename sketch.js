31f4ec421becdc1beaed56122473741d54de40cf
Tatyana Lee <Tatyana Lee>
2016-09-01 10:18:22 -0500

    yellow background

--- sketch.js
+++ sketch.js
@@ -1,20 +1,7 @@
 function setup() {
-  creativeCanvas(400, 269);
+  createCanvas(400, 269);
 }
 
 function draw() {
-  background(200);
-
-  ellipse(10, 10, 20);
-  ellipseMode(CENTER)
-  fill(225);
-
-  rect (20, 20, 20, 100);
-  fill(0, 255, 0);
-
-  rect(45, 20, 20, 100);
-  fill(255, 0, 255);
-
-  rect(80, 20, 20, 100);
-  fill(255);
+  background(255, 204, 0);
 }
