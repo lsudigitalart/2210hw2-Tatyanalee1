ffa57e049bbad2b827494ae3b82abf1c29d1c938
Tatyana Lee <Tatyana Lee>
2016-09-01 10:59:35 -0500

    Lines

--- sketch.js
+++ sketch.js
@@ -4,4 +4,16 @@ function setup() {
 
 function draw() {
   background(255, 204, 0);
+
+  stroke(0);
+  strokeWeight(2);
+  line(350, 100, 400, 100);
+
+  stroke(0);
+  strokeWeight(5);
+  line(350, 180, 400, 180);
+
+  stroke(0);
+  strokeWeight(8);
+  line(300, 260, 400, 260);
 }
